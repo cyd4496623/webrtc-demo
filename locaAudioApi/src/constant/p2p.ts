0677fd5b5fc81a302d8024728df44543969b9979
chenyunda <chenyunda@bhb.tv>
2023-05-02 00:12:36 +0800

    feat: 新增coturn服务配置

--- locaAudioApi/src/constant/p2p.ts
+++ locaAudioApi/src/constant/p2p.ts
@@ -1,5 +1,17 @@
 export const roomId = 1;
 
 export const config = {
-  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' },
+    {
+      urls: 'turn:8.134.67.109:3478',
+      username: 'cyd',
+      credential: '123123',
+    },
+    {
+      urls: 'turns:8.134.67.109:5349',
+      username: 'cyd',
+      credential: '123123',
+    },
+  ],
 };
