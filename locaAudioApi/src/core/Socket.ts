f1767a359d3ffd6e50700104dde1da20ef4fbe88
chenyunda <chenyunda@bhb.tv>
2023-05-16 18:57:16 +0800

    修改路径

--- locaAudioApi/src/core/Socket.ts
+++ locaAudioApi/src/core/Socket.ts
@@ -1,7 +1,7 @@
 import io, { Socket } from 'socket.io-client';
 
 export const createSocket = (query: Record<string, any>): Socket => {
-  return io('https://8.134.67.109:18080', {
+  return io('https://111.230.110.43:18080', {
     reconnectionDelayMax: 10000,
     transports: ['websocket'],
     query,
