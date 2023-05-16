9a20b2ceb2a9517815a89435f40ea840f403c986
chenyunda <chenyunda@bhb.tv>
2023-05-16 20:45:50 +0800

    fix: 修改coturn地址

--- locaAudioApi/src/constant/p2p.ts
+++ locaAudioApi/src/constant/p2p.ts
@@ -3,15 +3,19 @@ export const roomId = 1;
 export const config = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
+    // {
+    //   urls: 'turn:8.134.67.109:3478',
+    //   username: 'cyd',
+    //   credential: '123123',
+    // },
+    // {
+    //   urls: 'turns:8.134.67.109:5349',
+    //   username: 'cyd',
+    //   credential: '123123',
+    // },
     {
-      urls: 'turn:8.134.67.109:3478',
-      username: 'cyd',
-      credential: '123123',
-    },
-    {
-      urls: 'turns:8.134.67.109:5349',
-      username: 'cyd',
-      credential: '123123',
+      urls: 'turn:111.230.110.43:3478',
+      username: 'cyd-demo',
     },
   ],
 };
